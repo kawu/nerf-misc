961df335c71393567ff1280897e84ae6ef76e4b6
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-04-30 11:18:08 +0200

    Annotation tree data type

---
+++ src/Data/AnnTree.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Data.AnnTree
+( Tree (..)
+, Forest
+, drawTree
+, drawForest
+) where
+
+-- | @AnnTree a b@ represents a tree with @a@ annotation values in internal
+-- nodes and @b@ values in leaves.
+--
+-- Invariants not enforced by data structure:
+-- * Node has non-empty list of children
+data Tree a b = Node a (Forest a b)
+              | Leaf b
+              deriving (Show)
+type Forest a b = [Tree a b]
+
+class (Show a) => Label a where
+    label :: a -> String
+
+instance Label String where label x = x
+instance Label Char where label x = [x]
+instance (Show a) => Label a where label = show
+
+drawTree :: (Label a, Label b) => Tree a b -> String
+drawTree (Leaf x) = label x
+drawTree (Node x f) =
+    let indent = unlines . map ("  "++) . lines
+    in  concat (label x : "\n" : map (indent.drawTree) f)
+
+drawForest :: (Label a, Label b) => Forest a b -> String
+drawForest = unlines . map drawTree
