e1759c9e12956d2887c9d4f488641a9ef830dc73
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-06-22 11:54:42 +0200

    QuickCheck property for standard maximum computation

--- src/Proof/Tree.hs
+++ src/Proof/Tree.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Proof.Tree
 ( Pos
 , Tree (..)
@@ -12,8 +14,13 @@ module Proof.Tree
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.MemoTrie as Memo
+import Data.List (intercalate)
+import Control.Monad (forM_)
+import Control.Monad.Writer (execWriter, tell)
 import Control.Applicative ((<$>), (<*>))
 import Test.QuickCheck
+
+import Debug.Trace (trace)
 
 nub :: Ord a => [a] -> [a]
 nub = S.toList . S.fromList
@@ -25,28 +32,41 @@ arbitrarySet kMax gen = do
     nub <$> vectorOf k gen
 
 -- | QuickCheck parameters.
-posMax   = 5
-labelMax = 5
+posMax   = 6
+labelMax = 4
 ruleMax  = 10
-phiMax   = 10
-valMax   = 5.0
+phiMax   = 10.0
+descMax  = 100
 
--- | Position in a sentence.
+-- | Position in a sentence (positive).
 type Pos = Int
 
-arbitraryPos :: Pos -> Gen Pos
-arbitraryPos kMax = choose (1, kMax)
+-- | Value of potential (in logarithmic scale).
+type Phi = Double
 
-arbitraryValue :: Gen Double
-arbitraryValue = choose (-valMax, valMax)
+-- | Multiplication in logarithmic scale.
+(.*.) :: Phi -> Phi -> Phi
+(.*.) = (+)
+
+arbitraryPos :: Gen Pos
+arbitraryPos = choose (1, posMax)
+
+arbitraryRan :: Gen (Pos, Pos)
+arbitraryRan =
+    let pair = (,) <$> arbitraryPos <*> arbitraryPos
+    in  pair `suchThat` \(i, j) -> i <= j
+
+arbitraryValue :: Gen Phi
+arbitraryValue = choose (-phiMax, phiMax)
 
 -- | Binary tree.
-data Tree a = Branch { label  :: a
-                     , leftT  :: (Tree a)
-                     , rightT :: (Tree a) }
-            | Leaf   { label  :: a
-                     , pos    :: Pos }
-            deriving Show
+data Tree a
+    = Branch { label  :: a
+             , leftT  :: (Tree a)
+             , rightT :: (Tree a) }
+    | Leaf   { label  :: a
+             , pos    :: Pos }
+    deriving Show
 
 size :: Tree a -> Int
 size Leaf{} = 1
@@ -54,7 +74,7 @@ size (Branch _ l r) = 1 + size l + size r
 
 instance Arbitrary a => Arbitrary (Tree a) where
     arbitrary = do
-        n <- arbitraryPos posMax
+        n <- arbitraryPos
         arbitraryOn (1, n)
       where
         arbitraryOn (p, q)
@@ -64,6 +84,32 @@ instance Arbitrary a => Arbitrary (Tree a) where
                 Branch <$> arbitrary
                        <*> arbitraryOn (p, k)
                        <*> arbitraryOn (k+1, q)
+
+-- | Tree adorned with position information.
+data TreeP a
+    = BranchP { labelP :: a
+              , leftP  :: TreeP a
+              , rightP :: TreeP a
+              , ranP   :: (Pos, Pos) }
+    | LeafP   { labelP :: a
+              , posP   :: Pos } 
+    deriving Show
+
+begP :: TreeP a -> Pos
+begP LeafP{..}   = posP
+begP BranchP{..} = fst ranP
+
+endP :: TreeP a -> Pos
+endP LeafP{..}   = posP
+endP BranchP{..} = snd ranP
+
+mkTreeP :: Tree a -> TreeP a
+mkTreeP Leaf{..}   = LeafP label pos
+mkTreeP Branch{..} = BranchP label leftP rightP (p, q)
+  where
+    leftP  = mkTreeP leftT
+    rightP = mkTreeP rightT
+    (p, q) = (begP leftP, endP rightP)
 
 data Rule a = Rule
     { left  :: a
@@ -80,35 +126,79 @@ arbitraryRule xs =
     in  Rule <$> x <*> x<*> x
 
 -- | FIXME: this is incorrect definition!!!!
-arbitraryRulePos :: Pos -> Gen RulePos
-arbitraryRulePos kMax = (,,)
-    <$> arbitraryPos kMax
-    <*> arbitraryPos kMax
-    <*> arbitraryPos kMax
+arbitraryRulePos :: Gen RulePos
+arbitraryRulePos = (,,) <$> arbitraryPos <*> arbitraryPos <*> arbitraryPos
 
 data Nerf a = Nerf
     { labels  :: [a]
     , perTop  :: a -> [Rule a]
-    , phiBase :: Pos -> a -> Double
-    , phiRule :: RulePos -> Rule a -> Double }
+    -- | Potential values (in logarithmic scale).
+    , phiBase :: Pos -> a -> Phi
+    , phiRule :: RulePos -> Rule a -> Phi }
+
+phiTree :: Nerf a -> Tree a -> Phi
+phiTree nerf = phiTreeP nerf . mkTreeP
+
+phiTreeP :: Nerf a -> TreeP a -> Phi
+phiTreeP nerf LeafP{..} = phiBase nerf posP labelP
+phiTreeP nerf root
+    = phiRule nerf rulePos rule
+    .*. phiTreeP nerf left
+    .*. phiTreeP nerf right
+  where
+    rulePos = (begP left, endP left, endP right)
+    rule    = Rule (labelP left) (labelP root) (labelP right)
+    left    = leftP root
+    right   = rightP root
 
 data NerfDesc a = NerfDesc
     { labelsD  :: [a]
     , perTopD  :: M.Map a [Rule a]
-    , phiBaseD :: M.Map (Pos, a) Double
-    , phiRuleD :: M.Map (RulePos, Rule a) Double }
-    deriving Show
+    , phiBaseD :: M.Map (Pos, a) Phi
+    , phiRuleD :: M.Map (RulePos, Rule a) Phi }
+
+instance Show a => Show (NerfDesc a) where
+    show NerfDesc{..} = execWriter $ do
+        tell $ "labels: " ++ show labelsD
+        tell "\n"
+
+        tell "\nrules:\n"
+        forM_ (concat $ M.elems perTopD) $ \rule -> do
+            tell (show rule) >> tell "\n"
+
+        tell "\nphiBase:\n"
+        forM_ (M.toList phiBaseD) $ \(x, y) -> do
+            tell (show x)
+            tell " => " 
+            tell (show y)
+            tell "\n"
+
+        tell "\nphiRule:\n"
+        forM_ (M.toList phiRuleD) $ \(x, y) -> do
+            tell (show x)
+            tell " => " 
+            tell (show y)
+            tell "\n"
 
 nerfFromDesc :: Ord a => NerfDesc a -> Nerf a
-nerfFromDesc = undefined
+nerfFromDesc NerfDesc{..} = Nerf labels perTop phiBase phiRule
+  where
+    labels = labelsD
+    perTop x = case x `M.lookup` perTopD of
+        Just xs -> xs
+        Nothing -> []
+    phiBase = phiWith phiBaseD
+    phiRule = phiWith phiRuleD
+    phiWith :: (Ord a, Ord b) => M.Map (a, b) Phi -> a -> b -> Phi
+    phiWith phiMap i x = case (i, x) `M.lookup` phiMap of
+        Just v  -> v
+        Nothing -> 0.0  -- log 1.0
 
-arbitraryPhiBase :: Pos -> [a] -> Gen (Pos, a)
-arbitraryPhiBase kMax labels =
-    (,) <$> arbitraryPos kMax <*> elements labels 
+arbitraryPhiBase :: [a] -> Gen (Pos, a)
+arbitraryPhiBase labels = (,) <$> arbitraryPos <*> elements labels 
 
-arbitraryPhiRule :: Pos -> [a] -> Gen (RulePos, Rule a)
-arbitraryPhiRule kMax labels =
-    (,) <$> arbitraryRulePos kMax <*> arbitraryRule labels
+arbitraryPhiRule :: [a] -> Gen (RulePos, Rule a)
+arbitraryPhiRule labels = (,) <$> arbitraryRulePos <*> arbitraryRule labels
 
 instance (Ord a, Arbitrary a) => Arbitrary (NerfDesc a) where
     arbitrary = do
@@ -119,15 +209,14 @@ instance (Ord a, Arbitrary a) => Arbitrary (NerfDesc a) where
         phiRule <- phiWith labels arbitraryPhiRule
         return $ NerfDesc labels perTop phiBase phiRule
       where
-        phiWith :: Ord b => [a] -> (Pos -> [a] -> Gen b)
-                -> Gen (M.Map b Double)
+        phiWith :: Ord b => [a] -> ([a] -> Gen b) -> Gen (M.Map b Phi)
         phiWith labels gen = do
-            k <- choose (0, phiMax)
-            xs <- vectorOf k $ gen posMax labels
+            k <- choose (0, descMax)
+            xs <- vectorOf k $ gen labels
             vs <- vectorOf k $ arbitraryValue
             return $ M.fromList $ zip xs vs
 
--- | Build recursively a set of trees T using given rules set.
+-- | Build recursively a set of trees T using given nerf definition.
 treeSet :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> [Tree a]
 treeSet nerf = Memo.memo3 treeSet'
   where
@@ -141,25 +230,55 @@ treeSet nerf = Memo.memo3 treeSet'
             , t_r <- treeSet nerf (k+1) j (right r) ]
         | otherwise = error "treeSet: i > j"
 
--- alpha :: (Ord a, Memo.HasTrie a) => RuleSet a -> Pos -> Pos -> a -> Double
--- alpha rules = Memo.memo3 alpha'
---   where
---     alpha' i j x
---         | i == j = phiBase i x
---         | i < j  = maximum
---             [     phiRule (i, k, j) r
---               .*. alpha rules i     k (left r)
---               .*. alpha rules (k+1) j (right r)
---             | r <- justList (x `M.lookup` perTop rules)
---             , k <- [i..j-1] ]
---         | otherwise = error "alpha: i > j"
--- 
--- alpha' :: (Ord a, Memo.HasTrie a) => RuleSet a -> Pos -> Pos -> a -> Double
--- alpha' rules i j x = maximum $ map phiTree $ treeSet rules i j x
--- 
--- phiTree = undefined
--- phiBase = undefined
--- phiRule = undefined
--- 
--- (.*.) :: Num a => a -> a -> a
--- (.*.) = (+)
+alpha :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> Maybe Phi
+alpha nerf = Memo.memo3 alpha'
+  where
+    alpha' i j x
+        | i == j = Just $ phiBase nerf i x
+        | i < j  = maximumM
+            [ Just (phiRule nerf (i, k, j) r)
+              .?. alpha nerf i     k (left r)
+              .?. alpha nerf (k+1) j (right r)
+            | r <- perTop nerf x
+            , k <- [i..j-1] ]
+        | otherwise = error "alpha: i > j"
+    x .?. y = (.*.) <$> x <*> y
+    maximumM :: Ord a => [Maybe a] -> Maybe a
+    maximumM [] = Nothing
+    maximumM xs = maximum xs
+
+alpha' :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> Maybe Phi
+alpha' nerf i j x = case null trees of
+    False -> Just $ maximum $ map (phiTree nerf) trees
+    True  -> Nothing
+  where
+    trees = treeSet nerf i j x
+
+------------------------------------------------------------------------------
+
+data TestPoint a = TestPoint
+    { testNerf  :: NerfDesc a
+    , testComp :: (Pos, Pos, a) }
+
+instance Show a => Show (TestPoint a) where
+    show TestPoint{..} = execWriter $ do
+        tell "=== nerf ===\n"
+        tell $ show testNerf 
+        tell "\n=== computation ==\n"
+        tell $ show testComp
+
+instance (Ord a, Arbitrary a) => Arbitrary (TestPoint a) where
+    arbitrary = do
+        nerfDesc <- arbitrary
+        (i, j) <- arbitraryRan
+        x <- elements $ labelsD nerfDesc
+        return $ TestPoint nerfDesc (i, j, x)
+
+propAlpha :: (Show a, Ord a, Memo.HasTrie a) => TestPoint a -> Bool
+propAlpha test =
+    trace (show (y, y')) (y == y')
+  where
+    y  = alpha  nerf i j x
+    y' = alpha' nerf i j x
+    nerf = nerfFromDesc $ testNerf test
+    (i, j, x) = testComp test
