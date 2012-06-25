363a7b35008a125d1b3e3aadce921a85f6e7ab71
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-06-25 13:11:25 +0200

    QuickCheck property for beta computation

--- src/Proof/Tree.hs
+++ src/Proof/Tree.hs
@@ -11,17 +11,21 @@ module Proof.Tree
 , treeSet
 , alpha
 , alpha'
+, beta
+, beta'
 , propAlpha
+, propBeta
 ) where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.MemoTrie as Memo
 import Data.List (intercalate)
+import Data.Maybe (maybeToList)
 import Control.Monad (forM_)
 import Control.Monad.Writer (execWriter, tell)
-import Control.Applicative ((<$>), (<*>))
-import Test.QuickCheck
+import Control.Applicative ((<$>), (<*>), (<|>))
+import Test.QuickCheck hiding (label, labels)
 
 import Debug.Trace (trace)
 
@@ -50,6 +54,10 @@ type Phi = Double
 -- | Multiplication in logarithmic scale.
 (.*.) :: Phi -> Phi -> Phi
 (.*.) = (+)
+
+-- | Division in logarithmic scale.
+(./.) :: Phi -> Phi -> Phi
+(./.) = (-)
 
 arbitraryPos :: Gen Pos
 arbitraryPos = choose (1, posMax)
@@ -106,6 +114,9 @@ endP :: TreeP a -> Pos
 endP LeafP{..}   = posP
 endP BranchP{..} = snd ranP
 
+spanP :: TreeP a -> (Pos, Pos)
+spanP = (,) <$> begP <*> endP
+
 mkTreeP :: Tree a -> TreeP a
 mkTreeP Leaf{..}   = LeafP label pos
 mkTreeP Branch{..} = BranchP label leftP rightP (p, q)
@@ -113,6 +124,10 @@ mkTreeP Branch{..} = BranchP label leftP rightP (p, q)
     leftP  = mkTreeP leftT
     rightP = mkTreeP rightT
     (p, q) = (begP leftP, endP rightP)
+
+unTreeP :: TreeP a -> Tree a
+unTreeP LeafP{..}  = Leaf labelP posP
+unTreeP BranchP{..} = Branch labelP (unTreeP leftP) (unTreeP rightP)
 
 data Rule a = Rule
     { left  :: a
@@ -134,11 +149,13 @@ arbitraryRulePos =
     in  triplet `suchThat` \(i, k, j) -> i <= k && k < j
 
 data Nerf a = Nerf
-    { labels  :: [a]
-    , perTop  :: a -> [Rule a]
+    { labels   :: [a]
+    , perTop   :: a -> [Rule a]
+    , perLeft  :: a -> [Rule a]
+    , perRight :: a -> [Rule a]
     -- | Potential values (in logarithmic scale).
-    , phiBase :: Pos -> a -> Phi
-    , phiRule :: RulePos -> Rule a -> Phi }
+    , phiBase  :: Pos -> a -> Phi
+    , phiRule  :: RulePos -> Rule a -> Phi }
 
 phiTree :: Nerf a -> Tree a -> Phi
 phiTree nerf = phiTreeP nerf . mkTreeP
@@ -157,7 +174,7 @@ phiTreeP nerf root
 
 data NerfDesc a = NerfDesc
     { labelsD  :: [a]
-    , perTopD  :: M.Map a [Rule a]
+    , rulesD   :: [Rule a]
     , phiBaseD :: M.Map (Pos, a) Phi
     , phiRuleD :: M.Map (RulePos, Rule a) Phi }
 
@@ -167,7 +184,7 @@ instance Show a => Show (NerfDesc a) where
         tell "\n"
 
         tell "\nrules:\n"
-        forM_ (concat $ M.elems perTopD) $ \rule -> do
+        forM_ rulesD $ \rule -> do
             tell (show rule) >> tell "\n"
 
         tell "\nphiBase:\n"
@@ -185,15 +202,26 @@ instance Show a => Show (NerfDesc a) where
             tell "\n"
 
 nerfFromDesc :: Ord a => NerfDesc a -> Nerf a
-nerfFromDesc NerfDesc{..} = Nerf labels perTop phiBase phiRule
+nerfFromDesc NerfDesc{..} =
+    Nerf labels perTop perLeft perRight phiBase phiRule
   where
     labels = labelsD
-    perTop x = case x `M.lookup` perTopD of
-        Just xs -> xs
-        Nothing -> []
+
+    topM = mapWith top
+    leftM = mapWith left
+    rightM = mapWith right
+
+    perTop = perWith topM
+    perLeft = perWith leftM
+    perRight = perWith rightM
+
     phiBase = phiWith phiBaseD
     phiRule = phiWith phiRuleD
-    phiWith :: (Ord a, Ord b) => M.Map (a, b) Phi -> a -> b -> Phi
+
+    mapWith f = M.fromListWith (++) [(f x, [x]) | x <- rulesD]
+    perWith m x = case x `M.lookup` m of
+        Just xs -> xs
+        Nothing -> []
     phiWith phiMap i x = case (i, x) `M.lookup` phiMap of
         Just v  -> v
         Nothing -> 0.0  -- log 1.0
@@ -208,10 +236,9 @@ instance (Ord a, Arbitrary a) => Arbitrary (NerfDesc a) where
     arbitrary = do
         labels <- arbitrarySet labelMax arbitrary
         rules  <- arbitrarySet ruleMax (arbitraryRule labels)
-        let perTop = M.fromListWith (++) [(top x, [x]) | x <- rules]
         phiBase <- phiWith labels arbitraryPhiBase
         phiRule <- phiWith labels arbitraryPhiRule
-        return $ NerfDesc labels perTop phiBase phiRule
+        return $ NerfDesc labels rules phiBase phiRule
       where
         phiWith :: Ord b => [a] -> ([a] -> Gen b) -> Gen (M.Map b Phi)
         phiWith labels gen = do
@@ -221,7 +248,7 @@ instance (Ord a, Arbitrary a) => Arbitrary (NerfDesc a) where
             return $ M.fromList $ zip xs vs
 
 -- | Build recursively a set of trees T using given nerf definition.
-treeSet :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> [Tree a]
+treeSet :: Nerf a -> Pos -> Pos -> a -> [Tree a]
 treeSet nerf i j x
     | i == j = [Leaf x i]
     | i < j  =
@@ -232,50 +259,109 @@ treeSet nerf i j x
         , t_r <- treeSet nerf (k+1) j (right r) ]
     | otherwise = error "treeSet: i > j"
 
-alpha :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> Maybe Phi
-alpha nerf = Memo.memo3 alpha'
+-- | Find subtree with a given span and root value.
+subTree :: Eq a => Pos -> Pos -> Tree a -> Maybe (Tree a) 
+subTree i j tree = unTreeP <$> subTreeP i j (mkTreeP tree)
+
+-- | Find subtree with a given span.
+subTreeP :: Eq a => Pos -> Pos -> TreeP a -> Maybe (TreeP a)
+subTreeP i j tree
+    | (p, q) == (i, j) = Just tree
+    | p <= i && j <= q =
+        subTreeP i j (leftP tree) <|>
+        subTreeP i j (rightP tree)
+    | otherwise = Nothing
   where
+    (p, q) = spanP tree
+
+alpha :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> Maybe Phi
+alpha nerf = alphaM
+  where
+    alphaM = Memo.memo3 alpha'
     alpha' i j x
         | i == j = Just $ phiBase nerf i x
         | i < j  = maximumM
-            [     alphaNerf i     k (left r)
-              .?. alphaNerf (k+1) j (right r)
+            [     alphaM i     k (left r)
+              .?. alphaM (k+1) j (right r)
               .?. Just (phiRule nerf (i, k, j) r)
             | r <- perTop nerf x
             , k <- [i..j-1] ]
         | otherwise = error "alpha: i > j"
     x .?. y = (.*.) <$> x <*> y
-    maximumM :: Ord a => [Maybe a] -> Maybe a
-    maximumM [] = Nothing
-    maximumM xs = maximum xs
-    alphaNerf = alpha nerf  -- ^ No memoization without this trick
 
-alpha' :: (Ord a, Memo.HasTrie a) => Nerf a -> Pos -> Pos -> a -> Maybe Phi
-alpha' nerf i j x = case null trees of
-    False -> Just $ maximum $ map (phiTree nerf) trees
-    True  -> Nothing
+alpha' :: Ord a => Nerf a -> Pos -> Pos -> a -> Maybe Phi
+alpha' nerf i j x = catchNull maximum 
+    [phiTree nerf t | t <- treeSet nerf i j x]
+
+beta :: (Ord a, Memo.HasTrie a) => Nerf a
+     -> Pos -> Pos -> Pos -> a -> Maybe Phi
+beta nerf n i j x = case alphaM i j x of
+    Just _  -> betaM i j x
+    Nothing -> Nothing
   where
-    trees = treeSet nerf i j x
+    betaM = Memo.memo3 beta'
+    beta' i j x
+        | i == 1 && j == n =
+            case alphaM i j x of
+                Just _  -> Just 0.0     -- log 1.0
+                Nothing -> Nothing
+        | i >= 1 && j <= n && i <= j = maximumM
+            [ maximumM
+                [     alphaM k (i-1) (left r)
+                  .?. betaM  k j     (top r) 
+                  .?. Just (phiRule nerf (k, i-1, j) r)
+                | r <- perRight nerf x
+                , k <- [1 .. i-1] ]
+            , maximumM
+                [     alphaM (j+1) k (right r)
+                  .?. betaM  i     k (top r)
+                  .?. Just (phiRule nerf (i, j, k) r)
+                | r <- perLeft nerf x
+                , k <- [j+1 .. n] ] ]
+        | otherwise = error "beta: bad arguments"
+    x .?. y = (.*.) <$> x <*> y
+    alphaM = alpha nerf
+
+beta' :: Ord a => Nerf a -> Pos -> Pos -> Pos -> a -> Maybe Phi
+beta' nerf n i j x = catchNull maximum
+    [ phiTree nerf t ./. phiTree nerf t'
+    | y  <- labels nerf
+    , t  <- treeSet nerf 1 n y
+    , t' <- maybeToList $ subTree i j t
+    , label t' == x ]
+
+catchNull :: ([a] -> b) -> [a] -> Maybe b
+catchNull f xs
+    | null xs   = Nothing
+    | otherwise = Just $ f xs
+
+maximumM :: Ord a => [Maybe a] -> Maybe a
+maximumM [] = Nothing
+maximumM xs = maximum xs
 
 ------------------------------------------------------------------------------
 
 data TestPoint a = TestPoint
-    { testNerf  :: NerfDesc a
+    { testNerf :: NerfDesc a
+    , testSize :: Pos
     , testComp :: (Pos, Pos, a) }
 
 instance Show a => Show (TestPoint a) where
     show TestPoint{..} = execWriter $ do
         tell "=== nerf ===\n"
         tell $ show testNerf 
+        tell "\n=== test size ==\n"
+        tell $ show testSize
         tell "\n=== computation ==\n"
         tell $ show testComp
 
 instance (Ord a, Arbitrary a) => Arbitrary (TestPoint a) where
     arbitrary = do
         nerfDesc <- arbitrary
-        (i, j) <- arbitraryRan
+        n <- arbitraryPos
+        (i, j) <- arbitraryRan `suchThat` \(i, j) -> j <= n
         x <- elements $ labelsD nerfDesc
-        return $ TestPoint nerfDesc (i, j, x)
+        return $ TestPoint nerfDesc n (i, j, x)
 
 propAlpha :: (Show a, Ord a, Memo.HasTrie a) => TestPoint a -> Bool
 propAlpha test =
@@ -283,6 +369,16 @@ propAlpha test =
   where
     y  = alpha  nerf i j x
     y' = alpha' nerf i j x
+    nerf = nerfFromDesc $ testNerf test
+    (i, j, x) = testComp test
+
+propBeta :: (Show a, Ord a, Memo.HasTrie a) => TestPoint a -> Bool
+propBeta test =
+    trace (show (y, y')) (y ~== y')
+  where
+    y  = beta  nerf n i j x
+    y' = beta' nerf n i j x
+    n  = testSize test
     nerf = nerfFromDesc $ testNerf test
     (i, j, x) = testComp test
 
