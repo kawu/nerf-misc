5a903d5226469ff6bf1abaef919b7daa997bd003
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-06-27 13:18:21 +0200

    Initial version of Proof.Forest module

---
+++ src/Proof/Forest.hs
@@ -0,0 +1,27 @@
+module Proof.Forest
+( Forest
+, forestSet
+) where
+
+import qualified Data.MemoTrie as Memo
+
+import Proof.Tree
+
+-- | Invariant: trees in a forest are preserved in ascending order
+-- (order defined with respect to leaf positions).
+type Forest a = [Tree a]
+
+-- | Recursively build a forest set per given span.
+forestSet :: Active a -> Nerf a -> Pos -> Pos -> [Forest a]
+forestSet active nerf i j = map reverse $ recSet i j
+  where
+    recSet i j
+        | i > j     = [[]]
+        | otherwise = recSet i (j - 1) ++
+            [ t : f
+            | k <- [i..j]
+            , f <- recSet i (k-1)
+            , t <- treeSetSpan active nerf k j ]
+
+-- phiForest :: Nerf a -> Forest a -> Phi
+-- phiForest nerf f = sum [phiTree nerf t | t <- f]
